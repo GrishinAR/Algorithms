9b301a493abf9d231fe15ea91ec0da2a0b27a305
Grishin Alexandr <grishinarpz@gmail.com>
2017-11-25 01:31:41 +0300

    Реализация листов

--- src/data_structures/ListTwoLink.java
+++ src/data_structures/ListTwoLink.java
@@ -1,22 +1,29 @@
 package data_structures;
 
-
-public class ListOneLink<E> implements Data<E> {
+/**
+ * Реализация двухсвязного списка. Для наглядности toArray идёт по ссылке prev,
+ * из-за этого вывод будет в обратном порядке.
+ *
+ * @author Grishin A.R.
+ *
+ */
+public class ListTwoLink<E> implements Data<E> {
     private Node<E> bottom;
-    private Node<E> current;
+    private Node<E> top;
     private Node<E> element;
 
     private int size;
 
     @Override
     public E add(E e) {
-        element =  new Node<>(e, null);
         if(size == 0) {
+            element =  new Node<>(null, e, null);
             bottom = element;
         } else {
-            current.next = element;
+            element =  new Node<>(top, e, null);
+            top.next = element;
         }
-        current = element;
+        top = element;
         size++;
         return element.value;
     }
@@ -26,14 +33,13 @@ public class ListOneLink<E> implements Data<E> {
         if(size == 0) {
             throw new IndexOutOfBoundsException("Пустой лист");
         }
-        Node<E> element = bottom;
-        E returnElement = current.value;
-        while(element != null) {
-            if(element.next == current) {
-                element.next = null;
-                current = element;
-            }
-            element = element.next;
+        E returnElement = top.value;
+        if(size > 1) {
+            top.prev.next = null;
+            top = top.prev;
+        } else {
+            top = null;
+            bottom = null;
         }
         size--;
         return returnElement;
@@ -67,8 +73,10 @@ public class ListOneLink<E> implements Data<E> {
     private static class Node<E> {
         E value;
         Node next;
+        Node prev;
 
-        public Node(E value, Node next) {
+        public Node(Node prev, E value, Node next) {
+            this.prev = prev;
             this.value = value;
             this.next = next;
         }
